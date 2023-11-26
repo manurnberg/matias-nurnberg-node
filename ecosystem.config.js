577f8dcf5b5c3751b89a7e04ac8689c5fe3628eb
manurnberg <manurnbergy@gmail.com>
2023-11-25 22:13:10 -0300

    fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,10 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
+      NODE_ENV: 'development',
+      PORT: '80'
+    },
+    env: {
       NODE_ENV: 'production',
       PORT: '80'
     }
