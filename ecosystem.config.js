33c8fc692e2b16b9a3d4dd97557755814fe7906a
manurnberg <manurnbergy@gmail.com>
2023-11-25 22:22:27 -0300

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,21 @@
 const SUFFIX = process.argv.indexOf('--env') === -1 ? '' :
-      '-' + process.argv[process.argv.indexOf('--env')+1]
+      '-' + process.argv[process.argv.indexOf('--env')+1];
 
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'matias_nurnberg' + SUFFIX,
     script: 'src/index.js',
-    instaces: 2,
+    instances: 2,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
-      PORT: '80'
+      PORT: '3001'
     },
-    env: {
+    env_production: {
       NODE_ENV: 'production',
       PORT: '80'
     }
-    
-  },]
-
+  }]
 };
